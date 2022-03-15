0daa14b590dc43139fa83bf5c8fc77285ac7a34c
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2022-03-15 14:12:36 +0200

    Koodin dokumentointi

--- client.js
+++ client.js
@@ -1,20 +1,39 @@
 const memoize = require('memoizee');
 const fetch = require('node-fetch');
 
+/**
+ * Fetches and returns a JSON object containing Finnish postal codes as keys and district names
+ * as values.
+ * 
+ * @returns object of postal codes and districts, in the form {'00100': 'HELSINKI', ...}
+ */
 async function loadJson() {
     let response = await fetch('https://raw.githubusercontent.com/theikkila/postinumerot/master/postcode_map_light.json');
     return await response.json();
 }
 
-// Remember past results from loadJson for 60 seconds.
+// Remember (memoize) past results from loadJson for 60 seconds.
 // See https://www.npmjs.com/package/memoizee
 loadJson = memoize(loadJson, { maxAge: 60_000 });
 
-async function getPostalDistrict(postalCode) {
+
+/**
+ * Returns the name of the postal district that the given postal code belongs to.
+ * 
+ * @param {str} code such as '00100' 
+ * @returns District name such as 'HELSINKI' or null
+ */
+async function getPostalDistrict(code) {
     let data = await loadJson();
-    return data[postalCode] ?? null;
+    return data[code] ?? null;
 }
 
+/**
+ * Returns all postal codes that are in the given postal district (by name).
+ * 
+ * @param {str} district such as 'HELSINKI'
+ * @returns list of postal codes, ['00100', '00700']
+ */
 async function getPostalCodes(district) {
     let data = await loadJson();
     let codesAndNames = Object.entries(data);
